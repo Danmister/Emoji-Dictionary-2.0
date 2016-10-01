3e3efffdd91a3104f949085a32d1e496f3c562e8
Danmister <dcorrea012@gmail.com>
2016-10-01 18:36:44 -0400

    added ability to have a table view and have specifics along with it

--- Emoji Dictionary 2.0/ViewController.swift
+++ Emoji Dictionary 2.0/ViewController.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var emojiList: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        emojiList.dataSource = self
+        emojiList.delegate = self
     }
-
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
