21e83f53ab8f993ff26959c3b17cc4a2e749a539
Danmister <dcorrea012@gmail.com>
2016-10-01 16:35:52 -0400

    Started Emoji Dictionary 2.0

---
+++ Emoji Dictionary 2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary 2.0
+//
+//  Created by Daniel Correa on 10/1/16.
+//  Copyright © 2016 Random App Center. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
