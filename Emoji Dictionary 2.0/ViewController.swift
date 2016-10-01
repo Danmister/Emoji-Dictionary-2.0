77f2673434a1ec4c126c400a937293900fefca47
Danmister <dcorrea012@gmail.com>
2016-10-01 18:50:36 -0400

    Fixes

--- Emoji Dictionary 2.0/ViewController.swift
+++ Emoji Dictionary 2.0/ViewController.swift
@@ -20,7 +20,17 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         emojiList.delegate = self
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+        //asking how many cells, answer in return code
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Hello There"
+        return cell
+        //asking what kind of cell and repond with compiler genaric cell with string perviosily writen
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
