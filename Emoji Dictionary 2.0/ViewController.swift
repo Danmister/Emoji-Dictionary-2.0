7676c9e0d150beebc912e26a8528999c28b0c5f6
Danmister <dcorrea012@gmail.com>
2016-10-10 13:32:18 -0400

    added navigation controller

--- Emoji Dictionary 2.0/ViewController.swift
+++ Emoji Dictionary 2.0/ViewController.swift
@@ -36,6 +36,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         //asking what kind of cell and repond with compiler genaric cell with string perviosily writen
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "moveSegue", sender: "")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
